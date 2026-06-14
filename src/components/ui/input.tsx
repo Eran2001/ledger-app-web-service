834342c3433456cc8e120cbf88e18a54b17c19a3
M-E-Hasareli <eran.hasarali.2001@gmail.com>
2026-06-14 14:24:07 +0530

    add invalid state color to inout

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,10 +10,10 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       className={cn(
         "t-body text-main surface-transparent border-input-default global-rounded border",
         "h-field w-full min-w-0 px-3 py-1 no-shadow transition-[color,box-shadow] no-outline",
-        "file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium",
-        "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        "file:inline-flex file:h-small file:no-border file:surface-transparent file:t-input",
+        "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+        "focus-ring",
+        "invalid-state",
         className,
       )}
       {...props}
