8c7623f1a0199ee9248b3a9d802e789cf72b4869
M-E-Hasareli <eran.hasarali.2001@gmail.com>
2026-06-15 09:17:29 +0530

    enahnce otp compo

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,37 +1,38 @@
 import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupInput,
-  InputGroupText,
-} from "@/components/ui/input-group";
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+  InputOTPSeparator,
+} from "@/components/ui/input-otp";
 import { TopBar } from "@/components/shared/top-bar";
-import * as Icon from "@/components/icons";
 
 const TestPage = () => {
   return (
     <>
-      <TopBar pageTitle="Input Group" pageSubtitle="Test" />
-      <div className="p-6 flex flex-col gap-4 max-w-sm">
-        <InputGroup>
-          <InputGroupAddon align="inline-start">
-            <Icon.Search />
-          </InputGroupAddon>
-          <InputGroupInput placeholder="Search..." />
-        </InputGroup>
+      <TopBar pageTitle="Input OTP" pageSubtitle="Test" />
+      <div className="p-6 flex flex-col gap-6">
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
 
-        <InputGroup>
-          <InputGroupAddon align="inline-start">
-            <InputGroupText>https://</InputGroupText>
-          </InputGroupAddon>
-          <InputGroupInput placeholder="yoursite.com" />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroupInput placeholder="Amount" />
-          <InputGroupAddon align="inline-end">
-            <InputGroupText>USD</InputGroupText>
-          </InputGroupAddon>
-        </InputGroup>
+        <InputOTP maxLength={4}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+            <InputOTPSlot index={3} />
+          </InputOTPGroup>
+        </InputOTP>
       </div>
     </>
   );
