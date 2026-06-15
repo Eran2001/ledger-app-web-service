762bbc99f8de34f5fb44c27e0a408740db01740e
M-E-Hasareli <eran.hasarali.2001@gmail.com>
2026-06-15 10:29:25 +0530

    enahnce popover compo

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,45 +1,46 @@
 import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-  PaginationLink,
-  PaginationPrevious,
-  PaginationNext,
-  PaginationEllipsis,
-} from "@/components/ui/pagination";
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
 import { TopBar } from "@/components/shared/top-bar";
 
 const TestPage = () => {
   return (
     <>
-      <TopBar pageTitle="Pagination" pageSubtitle="Test" />
-      <div className="p-6 flex flex-col gap-8">
+      <TopBar pageTitle="Popover" pageSubtitle="Test" />
+      <div className="p-6 flex items-start gap-6">
 
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious href="#" />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#">1</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#" isActive>2</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#">3</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationEllipsis />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#">10</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationNext href="#" />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline">Open popover</Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <div className="flex flex-col gap-2">
+              <p className="t-meta fw-medium">Settings</p>
+              <p className="t-caption">Manage your account preferences here.</p>
+            </div>
+          </PopoverContent>
+        </Popover>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline">Align start</Button>
+          </PopoverTrigger>
+          <PopoverContent align="start">
+            <p className="t-meta">Aligned to start.</p>
+          </PopoverContent>
+        </Popover>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline">Align end</Button>
+          </PopoverTrigger>
+          <PopoverContent align="end">
+            <p className="t-meta">Aligned to end.</p>
+          </PopoverContent>
+        </Popover>
 
       </div>
     </>
